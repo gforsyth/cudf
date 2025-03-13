423796728e535f2fd5bb911c8cac1310f4e096c2
Gil Forsyth <gforsyth@nvidia.com>
2025-03-13 12:55:51 -0400

    fix up python/ shell scripts

--- python/cudf/cudf/pandas/scripts/run-pandas-tests.sh
+++ python/cudf/cudf/pandas/scripts/run-pandas-tests.sh
@@ -32,7 +32,7 @@ cd pandas-testing
 if [ ! -d "pandas" ]; then
     git clone https://github.com/pandas-dev/pandas
 fi
-cd pandas && git clean -fdx && git checkout v$PANDAS_VERSION && cd ../
+cd pandas && git clean -fdx && git checkout v"$PANDAS_VERSION" && cd ../
 
 
 if [ ! -d "pandas-tests" ]; then
@@ -68,16 +68,16 @@ EOF
     # Substitute `pandas.tests` with a relative import.
     # This will depend on the location of the test module relative to
     # the pandas-tests directory.
-    for hit in $(find . -iname '*.py' | xargs grep "pandas.tests" | cut -d ":" -f 1 | sort | uniq); do
+    for hit in $(find . -iname '*.py' -print0 | xargs -0 grep "pandas.tests" | cut -d ":" -f 1 | sort | uniq); do
         # Get the relative path to the test module
-        test_module=$(echo $hit | cut -d "/" -f 2-)
+        test_module=$(echo "$hit" | cut -d "/" -f 2-)
         # Get the number of directories to go up
-        num_dirs=$(echo $test_module | grep -o "/" | wc -l)
-        num_dots=$(($num_dirs - 2))
+        num_dirs=$(echo "$test_module" | grep -o "/" | wc -l)
+        num_dots=$((num_dirs - 2))
         # Construct the relative import
         relative_import=$(printf "%0.s." $(seq 1 $num_dots))
         # Replace the import
-        sed -i "s/pandas.tests/${relative_import}/g" $hit
+        sed -i "s/pandas.tests/${relative_import}/g" "$hit"
     done
 fi
 
@@ -139,9 +139,9 @@ PANDAS_CI="1" timeout 90m python -m pytest -p cudf.pandas \
     -v -m "not single_cpu and not db" \
     -k "$TEST_THAT_NEED_MOTO_SERVER and $TEST_THAT_CRASH_PYTEST_WORKERS and not test_groupby_raises_category_on_category and not test_constructor_no_pandas_array and not test_is_monotonic_na and not test_index_contains and not test_index_contains and not test_frame_op_subclass_nonclass_constructor and not test_round_trip_current and not test_pickle_frame_v124_unpickle_130" \
     --import-mode=importlib \
-    ${PYTEST_IGNORES} \
+    "${PYTEST_IGNORES}" \
     "$@" || [ $? = 1 ]  # Exit success if exit code was 1 (permit test failures but not other errors)
 
-mv *.json ..
+mv ./*.json ..
 cd ..
 rm -rf pandas-testing/pandas-tests/
