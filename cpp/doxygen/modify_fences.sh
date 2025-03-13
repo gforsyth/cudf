b82d0a8e0f3bebd2e693ec9fd208b27334e1e2c0
Gil Forsyth <gforsyth@nvidia.com>
2025-03-13 12:49:47 -0400

    fix up cpp/ shell scripts

--- cpp/doxygen/modify_fences.sh
+++ cpp/doxygen/modify_fences.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Copyright (c) 2022, NVIDIA CORPORATION.
+# Copyright (c) 2022-2025, NVIDIA CORPORATION.
 
 # This script modifies the GitHub Markdown style code fences in our MD files
 # into the PHP style that Doxygen supports, allowing us to display code
 # properly both on the GitHub GUI and in published Doxygen documentation.
 
+# shellcheck disable=SC2016  # shellcheck thinks this is variable expansion but it isn't
 sed 's/```c++/```{.cpp}/g' "$@"
